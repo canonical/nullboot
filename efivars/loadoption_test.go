6086a9501ed7ad905dfa5c024be63e57d7468419
Julian Andres Klode <julian.klode@canonical.com>
2021-08-25 20:57:01 +0200

    efivars: Add more loadoption tests

--- efivars/loadoption_test.go
+++ efivars/loadoption_test.go
@@ -6,9 +6,60 @@ package efivars
 
 import (
 	"bytes"
+	"reflect"
+	"regexp"
 	"testing"
 )
 
+var UsbrBootCdrom = []byte{9, 0, 0, 0, 28, 0, 85, 0, 83, 0, 66, 0, 82, 0, 32, 0, 66, 0, 79, 0, 79, 0, 84, 0, 32, 0, 67, 0, 68, 0, 82, 0, 79, 0, 77, 0, 0, 0, 2, 1, 12, 0, 208, 65, 3, 10, 0, 0, 0, 0, 1, 1, 6, 0, 0, 20, 3, 5, 6, 0, 11, 1, 127, 255, 4, 0}
+
+func TestNewLoadOptionFromVariable(t *testing.T) {
+	tests := []struct {
+		label string
+		input []byte
+		want  LoadOption
+		err   string
+	}{
+		{"USBR Boot CDROM", UsbrBootCdrom, LoadOption{UsbrBootCdrom}, ""},
+		{"Dummy {0,1}", []byte{0, 1}, LoadOption{}, "Invalid load option"},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.label, func(t *testing.T) {
+			got, err := NewLoadOptionFromVariable(tc.input)
+			if tc.err != "" {
+				if matched, _ := regexp.MatchString(tc.err, err.Error()); !matched {
+					t.Fatalf("expected error: %v, got: %v", tc.err, err)
+				}
+			}
+			if !reflect.DeepEqual(tc.want, got) {
+				t.Fatalf("expected: %v, got: %v", tc.want, got)
+			}
+		})
+
+	}
+}
+
+func TestLoadOptionDesc(t *testing.T) {
+	tests := []struct {
+		label string
+		input []byte
+		want  string
+	}{
+		{"USBR Boot CDROM", UsbrBootCdrom, "USBR BOOT CDROM"},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.label, func(t *testing.T) {
+			lo, _ := NewLoadOptionFromVariable(tc.input)
+			got := lo.Desc()
+			if !reflect.DeepEqual(tc.want, got) {
+				t.Fatalf("expected: %v, got: %v", tc.want, got)
+			}
+		})
+
+	}
+}
 func TestNewLoadOptionArgumentFromUTF8(t *testing.T) {
 	tests := []struct {
 		input string
