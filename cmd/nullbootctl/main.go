33bb9ed402f1b259b1e7d70bd6665f89ff5b6b66
Julian Andres Klode <julian.klode@canonical.com>
2022-04-29 19:26:55 +0200

    Add output-json flag to write EFI variables to given file

--- cmd/nullbootctl/main.go
+++ cmd/nullbootctl/main.go
@@ -11,6 +11,7 @@ import "os"
 
 var noTPM = flag.Bool("no-tpm", false, "Do not do any resealing with the TPM")
 var noEfivars = flag.Bool("no-efivars", false, "Do not use or update the EFI variables")
+var outputJSON = flag.String("output-json", "", "JSON file to write (also disables writing real EFI variables)")
 
 func main() {
 	var assets *efibootmgr.TrustedAssets
@@ -46,8 +47,14 @@ func main() {
 	}
 
 	var maybeBm *efibootmgr.BootManager
+	var efivars efibootmgr.EFIVariables
+	if *outputJSON != "" {
+		efivars = &efibootmgr.MockEFIVariables{}
+	} else {
+		efivars = efibootmgr.RealEFIVariables{}
+	}
 	if !*noEfivars {
-		if bm, err := efibootmgr.NewBootManagerFromSystem(); err != nil {
+		if bm, err := efibootmgr.NewBootManagerForVariables(efivars); err != nil {
 			log.Println("cannot load efi boot variables:", err)
 			os.Exit(1)
 		} else {
@@ -113,6 +120,27 @@ func main() {
 		// Final reseal to remove obsolete assets from profile
 		if err := efibootmgr.ResealKey(assets, km, esp, shimSourceDir, vendor); err != nil {
 			log.Println("final reseal failed:", err)
+			os.Exit(1)
+		}
+	}
+
+	if jsonEfivars := efivars.(*efibootmgr.MockEFIVariables); jsonEfivars != nil {
+		json, err := jsonEfivars.JSON()
+		if err != nil {
+			log.Println("cannot write json:", err)
+			os.Exit(2)
+		}
+
+		f, err := os.Create(*outputJSON)
+		if err != nil {
+			log.Printf("Could not open JSON output file %s: %v", *outputJSON, err)
+			os.Exit(1)
+		}
+		defer f.Close()
+
+		_, err = f.Write(json)
+		if err != nil {
+			log.Printf("Could not write JSON output file %s: %v", *outputJSON, err)
 			os.Exit(1)
 		}
 	}
