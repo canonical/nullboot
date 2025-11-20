17defc18c023244fba78ccf8f24e318aeec862d2
Michael Raymond <michael.raymond@canonical.com>
2025-11-20 16:48:47 -0500

    feat: CLI update to support fallback mechanism
    This change enables the kernel fallback mechanism as it relates to new
    kernels via `nullbootctl`. The new CLI flag disables the setting of
    BootNext such that an unsuccessful boot onto BootNext does not result
    in the continued setting of BootNext. Rather, the installation trigger
    will set BootNext and the systemd service executed on startup will not.
    
    Default behavior is to implement the fallback mechanism, and a successful
    boot onto that kernel will amend the boot loader. Unsuccessful boots
    will not amend the boot loader or set BootNext again until another apt
    event triggers `nullbootctl` again.

--- cmd/nullbootctl/main.go
+++ cmd/nullbootctl/main.go
@@ -10,8 +10,9 @@ import "log"
 import "os"
 
 var noTPM = flag.Bool("no-tpm", false, "Do not do any resealing with the TPM")
-var noEfivars = flag.Bool("no-efivars", false, "Do not use or update the EFI variables")
-var outputJSON = flag.String("output-json", "", "JSON file to write (also disables writing real EFI variables)")
+var noEfivars = flag.Bool("no-efivars", false, "Do not use or update the EFI variables. Disables kernel fallback mechanism")
+var outputJSON = flag.String("output-json", "", "JSON file to write. Disables writing real EFI variables and enablement of the kernel fallback mechanism")
+var noBootNext = flag.Bool("no-boot-next", false, "Disables use of BootNext. This flag must be disabled in order to upgrade to a new kernel version.")
 
 func main() {
 	var assets *efibootmgr.TrustedAssets
@@ -25,7 +26,7 @@ func main() {
 		vendor          = "ubuntu"
 	)
 
-	// FIXME: Let's actually add some arg parsing and stuff?
+	usingRealEFIVars := *outputJSON == "" && !*noEfivars
 	if !*noTPM {
 		assets, err = efibootmgr.ReadTrustedAssets()
 		if err != nil {
@@ -102,18 +103,50 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err = km.CommitToBootLoader(); err != nil {
-		log.Print(err)
-		os.Exit(1)
+	// Determine if the fallback mechanism is required
+	isCurrentBootLatest := true
+	if usingRealEFIVars {
+		// Only set fallback if the latest kernel is not booted
+		isCurrentBootLatest, err = km.IsCurrentBootLatest()
+		if err != nil {
+			log.Printf("Unable to determine if the latest kernel is BootCurrent: %v", err)
+			os.Exit(1)
+		}
+		log.Println("BootCurrent is not the latest installed kernel entry")
 	}
-	// Cleanup old entries
-	if err = km.RemoveObsoleteKernels(); err != nil {
-		log.Print(err)
-		os.Exit(1)
-	}
-	if err = km.CommitToBootLoader(); err != nil {
-		log.Print(err)
-		os.Exit(1)
+
+	// If current boot is not latest, set latest to BootNext so it can
+	// attempt to boot on next reboot
+	//
+	// Else, the current kernel booted successfully and the EFI variables
+	// can be updated accordingly; notably, the BootCurrent will become
+	// BootOrder[0]
+	if !isCurrentBootLatest {
+		if !*noBootNext {
+			if err := km.SetLatestKernelToBootNext(); err != nil {
+				log.Printf("Unable to set kernel fallback for new kernel: %v", err)
+				os.Exit(1)
+			}
+			log.Println("Set kernel fallback mechanism for newly installed kernel")
+		}
+	} else {
+		if err = km.CommitToBootLoader(); err != nil {
+			log.Print(err)
+			os.Exit(1)
+		}
+		// Cleanup old entries
+		if err = km.RemoveObsoleteKernels(); err != nil {
+			log.Print(err)
+			os.Exit(1)
+		}
+		// This second call is intended to cleanup Boot variables that
+		// become obsolete after the first call. It is not explicitly
+		// tested, but I am not convinced that it is necessary. Regardless,
+		// I do not want to break anything since I am not 100% sure of this
+		if err = km.CommitToBootLoader(); err != nil {
+			log.Print(err)
+			os.Exit(1)
+		}
 	}
 
 	if assets != nil {
@@ -129,7 +162,6 @@ func main() {
 			os.Exit(1)
 		}
 	}
-
 	if jsonEfivars, ok := efivars.(*efibootmgr.MockEFIVariables); ok {
 		json, err := jsonEfivars.JSON()
 		if err != nil {
